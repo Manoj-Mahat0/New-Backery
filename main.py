5e48a99bf7d4fbbc7eaf1c75311e5a1e4e7ebf09
Manoj-Mahat0 <codeinlastbench@gmail.com>
2025-06-06 18:02:45 +0530

    Cross

--- main.py
+++ main.py
@@ -3,11 +3,19 @@ from db import Base, engine  # <-- Add engine here
 from routes import auth, designer_order_routes, order_routes, store_routes
 from models.user import User  # This ensures table gets registered
 from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
 
 # Create all tables
 Base.metadata.create_all(bind=engine) # type: ignore
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Or specify allowed origins
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.mount("/media", StaticFiles(directory="media"), name="media")
 app.include_router(auth.router, prefix="/auth", tags=["Authentication"])
 app.include_router(store_routes.router, prefix="/stores", tags=["Stores"])

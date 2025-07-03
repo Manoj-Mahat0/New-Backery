fc2f621df2d3722f6d551578349e39ff04d25e76
Manoj Mahato <127175133+Manoj-Mahat0@users.noreply.github.com>
2025-07-03 13:13:35 +0530

    Update models.py
    store modls update

--- models/models.py
+++ models/models.py
@@ -3,18 +3,23 @@ from db import Base
 
 class MainStore(Base):
     __tablename__ = "main_store"
+    
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String)
-    location = Column(String)
+    name = Column(String(100), nullable=False)      # ✅ Add length
+    location = Column(String(255), nullable=True)   # ✅ Add length
+
 
 class Store(Base):
     __tablename__ = "stores"
+    
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String)
-    main_store_id = Column(Integer, ForeignKey("main_store.id"))
+    name = Column(String(100), nullable=False)             # ✅ Add length
+    main_store_id = Column(Integer, ForeignKey("main_store.id"), nullable=False)
+
 
 class Factory(Base):
     __tablename__ = "factories"
+    
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String)
-    main_store_id = Column(Integer, ForeignKey("main_store.id"))
+    name = Column(String(100), nullable=False)             # ✅ Add length
+    main_store_id = Column(Integer, ForeignKey("main_store.id"), nullable=False)
